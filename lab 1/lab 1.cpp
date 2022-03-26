b7fbe2141cc59427b296cb33756b408a74c47f77
Глеб Панченко <panchenko.glebka@gmail.com>
2022-03-26 17:21:23 +0500

    Добавьте файлы проекта.

---
+++ lab 1/lab 1.cpp
@@ -0,0 +1,30 @@
+﻿#include <GL/glut.h>
+#include <iostream>
+
+void RenderSceneCB() 
+{
+    glClear(GL_COLOR_BUFFER_BIT); // очистка вроде
+    glutSwapBuffers(); // меняю буфер используемый с заготовленным
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv); // инициализация глут
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+
+    glutInitWindowSize(1024, 768); // создаем окно с параметрами размер и позиции
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("ыыыыыыыыыы");
+
+    glutDisplayFunc(RenderSceneCB); // вызов функции 
+
+    glClearColor(0.6f, 0.2f, 0.3f, 0.4f); // смена цвета
+
+   
+
+   
+    glutMainLoop(); // ??? 
+
+
+}
+
